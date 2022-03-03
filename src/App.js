9e4593235fcbccffc6c984d6a9110287127f8be6
texaslou <texaslou@gmail.com>
2022-03-03 06:26:19 -0600

    update menu-items to navigate withRouter-higher order component-HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
+
+
+const HatsPage = ()=>(
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
+
+const JacketsPage = ()=>(
+  <div>
+    <h1>JACKETS PAGE</h1>
+  </div>
+
+)
+
 
 function App() {
   return (
     <div>
-     <HomePage />
+     <Switch>
+       <Route exact path = '/' component= {HomePage} />
+       <Route path='/shop/hats' component={HatsPage} />
+       <Route path='/shop/jackets' component={JacketsPage} />
+     </Switch>
     </div>
   );
 }
