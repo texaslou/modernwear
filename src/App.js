5ba804471fe705c9dd9a2ed7ef7d33771508f138
texaslou <texaslou@gmail.com>
2022-03-03 14:41:20 -0600

    added header, linked homepage, shoppage

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import './App.css'
+import Header from "./components/header/header.component";
 
 
 
@@ -12,6 +13,7 @@ import './App.css'
 function App() {
   return (
     <div className="App">
+      <Header />
      <Switch>
        <Route exact path = '/' component= {HomePage} />
        <Route path='/shop' component={ShopPage} />
