09b1db8d2dadf275a4c9ef93d446e52f1e088524
texaslou <texaslou@gmail.com>
2022-03-04 04:05:30 -0600

    applied firebase to app, created sign in page with sign in component and sign in with google

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import { auth } from '../../firebase/firebase.utils';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.styles.scss'
 
-const Header = ()=> {
+const Header = ({ currentUser })=> {
     return(
       <div className="header">
           <Link className="logo-container" to='/'>
@@ -12,6 +13,12 @@ const Header = ()=> {
           <div className="options">
               <Link className="option" to='/shop'>SHOP</Link>
               <Link className="option" to='/shop'>CONTACT</Link>
+              {
+                currentUser?
+                <div className="option" onClick={()=>auth.signOut()}>SIGN OUT</div>
+                :
+                <Link className="opton" to='/signin'>SIGN IN</Link>
+              }
           </div>
 
       </div>
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+const config = {
+    apiKey: "AIzaSyBSU-UzLF4q3nWrvKNiGovAoV-85hjey1M",
+    authDomain: "modernwear-35d51.firebaseapp.com",
+    projectId: "modernwear-35d51",
+    storageBucket: "modernwear-35d51.appspot.com",
+    messagingSenderId: "985373935938",
+    appId: "1:985373935938:web:7827591c5df3a7eb3560db",
+    measurementId: "G-L553X3E1EZ"
+  };
+
+  firebase.initializeApp(config)
+
+  export const auth = firebase.auth();
+  export const firestore = firebase.firestore();
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+  provider.setCustomParameters({prompt:'select_account'});
+  export const signInWithGoogle = ()=> auth.signInWithPopup(provider);
+
+  export default firebase;
+
+
